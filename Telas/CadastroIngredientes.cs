1ff06e7e9c0ef2b2e91892303e76e711c0087c57
Gabriel Tavares <gabrielgstell@gmail.com>
2023-05-10 20:35:40 -0300

    versão com a inserção de dados funcional

--- Telas/CadastroIngredientes.cs
+++ Telas/CadastroIngredientes.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Configuration;
 using PizzariaDoZe_DAO;
 using System.Configuration;
 
@@ -21,17 +20,12 @@ namespace PizzariaDoZe
         {
             InitializeComponent();
 
-            // pega os dados do banco de dados
-            string provider = ConfigurationManager.ConnectionStrings["BD"].ProviderName;
-            string strConnection = ConfigurationManager.ConnectionStrings["BD"].ConnectionString;
-            // cria a instancia da classe da model
-
-            dao = new IngredienteDAO(provider, strConnection);
+            dao = new IngredienteDAO();
 
             this.KeyDown += new KeyEventHandler(Funcoes.FormEventoKeyDown!);
 
 
-            crud1.Btn12_Salvar.Click += Btn12_Salvar_Click1;
+            crud1.Btn12_Salvar.Click += Btn12_Salvar_Click;
 
             //Ajuste do Foco campo Nome
             ING_NOME_TEXT.Enter += new
@@ -43,25 +37,6 @@ namespace PizzariaDoZe
             EventHandler(Funcoes.CampoEventoEnter!);
             ING_ID_TEXT.Leave += new
             EventHandler(Funcoes.CampoEventoLeave!);
-        }
-
-        private void Btn12_Salvar_Click1(object? sender, EventArgs e)
-        {
-            //Instância e Preenche o objeto com os dados da view
-            var ingrediente = new Ingrediente(int.Parse(ING_ID_TEXT.Text), "'"+ING_NOME_TEXT.Text+"'");
-
-            try
-            {
-                // chama o método para inserir da camada model
-                dao.InserirDbProvider(ingrediente);
-                MessageBox.Show("Dados inseridos com sucesso!");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-
-
         }
 
         private void VOLTAR_CAD_INGREDIENTES_Click(object sender, EventArgs e)
@@ -82,7 +57,8 @@ namespace PizzariaDoZe
         private void Btn12_Salvar_Click(object sender, EventArgs e)
         {
             //Instância e Preenche o objeto com os dados da view
-            var ingrediente = new Ingrediente(int.Parse(ING_ID_TEXT.Text), ING_NOME_TEXT.Text);
+            var ingrediente = new Ingrediente(ING_NOME_TEXT.Text);
+
 
             try
             {
